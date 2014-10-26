5b9d35733f2cfc7488c5f80a6f2362f506210a8e
arpane4c5 <arpan.e4c5@gmail.com>
2014-10-27 02:56:54 +0530

    FinalCommit

--- README.md
+++ README.md
@@ -2,5 +2,11 @@ CleaningDataProjectCoursera
 ===========================
 
 # Instructions
-The data should be in the directory "UCIDataset". You can simply rename the directory that you downloaded to "UCIDataset".
-The file run_analysis.R contains the code for cleaning the dataset. There are no parameters to the function analyseSets().
+1. Download the UCI HAR Dataset from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+2. Extract the data to the working directory.
+3. Rename the folder from "UCI HAR Dataset" to "UCIDataset".
+4. Load the run_analysis.R file using source() command.
+5. Call analyseSets(). This will read the required data files and create a "cleanData.txt" file in your working directory.
+
+# Cleaning Procedure
+The script takes the training and testing datasets, extracts the values corresponding to the mean and standard deviation values, adds columns for activity names and subject_ID and finally stores the cleaned data in a "cleanData.txt" file. 
--- run_analysis.R
+++ run_analysis.R
@@ -1,29 +1,37 @@
+# The function can be called simply by analyseSets()
 analyseSets<-function()
 {
-     #reading feature names and activity labels in a table
+     # reading feature names and activity labels
      featureNames<-read.table("./UCIDataset/features.txt",colClasses="character")
-     labels<-read.table("./UCIDataset/activity_labels.txt")
+     actLabel<-read.table("./UCIDataset/activity_labels.txt",colClasses="character")
+     # forming a character vector of the labels
+     actLabel<-actLabel$V2
      
-     subTrain<-read.table("./UCIDataset/train/subject_train.txt")
-     subTest<-read.table("./UCIDataset/test/subject_test.txt")
-     
+     # reading subject IDs and activity IDs of training and test data
+     subTrain<-read.table("./UCIDataset/train/subject_train.txt",colClasses="numeric")
+     subTest<-read.table("./UCIDataset/test/subject_test.txt",colClasses="numeric")
+     actTrain<-read.table("./UCIDataset/train/y_train.txt",colClasses="numeric")
+     actTest<-read.table("./UCIDataset/test/y_test.txt",colClasses="numeric")
+     # adding columns for activity labels corresponding to the activity IDs
+     actTrain[,2]<-actLabel[actTrain[,1]]
+     actTest[,2]<-actLabel[actTest[,1]]
      
      # reading the training and test data
-     trainSet<-read.table("./UCIDataset/train/X_train.txt",col.names=featureNames$V2,colClasses="numeric")
-     testSet<-read.table("./UCIDataset/test/X_test.txt",col.names=featureNames$V2,colClasses="numeric")
-     trainSet<-cbind(trainSet,subject=subTrain[,1])
-     testSet<-cbind(testSet,subject=subTest[,1])
-
+     setTrain<-read.table("./UCIDataset/train/X_train.txt",col.names=featureNames$V2,colClasses="numeric")
+     setTest<-read.table("./UCIDataset/test/X_test.txt",col.names=featureNames$V2,colClasses="numeric")
      
-     #merging both the datasets
-     mergedSet<-merge(trainSet,testSet)
-     
-     #required features
+     # extracting required features (mean and standard deviation values only)
      reqFeatureIndex<-grep(paste(c("mean","std"),collapse="|"),featureNames$V2)
+     setTrain<-setTrain[,reqFeatureIndex]
+     setTest<-setTest[,reqFeatureIndex]
      
+     # column binding the activity names and the subject IDs
+     setTrain<-cbind(subject_id=subTrain[,1],activity=actTrain[,2],setTrain)
+     setTest<-cbind(subject_id=subTest[,1],activity=actTest[,2],setTest)
      
-     reqSet<-mergedSet[,reqFeatureIndex]
-     reqSet<-cbind(reqSet,mergedSet$subject)
+     # merging both the datasets
+     cleanData<-rbind(setTrain, setTest)
      
-     reqSet
+     # writing the datasets to the text file named "cleanData.txt"
+     write.table(cleanData,file="./cleanData.txt",row.names=FALSE)
 }
